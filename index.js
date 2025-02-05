ed073696f3f2290b33ad5d40c19e2cfc75182d1d
Keely Tan <keely.tan94@gmail.com>
2025-02-05 21:17:07 +1100

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+document.getElementById("main").remove();
+
+const newHeader=document.createElement("h1");
+newHeader.id="victory";
+newHeader.textContent="Keely is the champion";
+document.body.append(newHeader);
+
+
+
+
+
+
+
+
+
